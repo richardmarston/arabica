f5d58cd0caf6fbb98df18d7fcc267e4111da5999
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-05-22 00:04:05 +0200

    input

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,3 @@ mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON ..
 cpack
-echo $(pwd)
-RPM=$(find build -maxdepth 1 -iname '*.rpm')
-DEB=$(find build -maxdepth 1 -iname '*.deb')
-echo "rpm: ${RPM}"
-echo "deb: ${DEB}"
